72ce2838c9c15375e7780076dfb8567f46450f77
Sanchit Aggarwal <46450398+sanchit0000@users.noreply.github.com>
2026-02-09 15:21:45 +0530

    Implement employee crud logic to pass initial test (Green Phase)

--- app/main.py
+++ app/main.py
@@ -23,3 +23,35 @@ def create_employee(employee: schemas.EmployeeCreate, db: Session = Depends(get_
     db.commit()
     db.refresh(db_emp)
     return db_emp
+
+
+@app.get("/employees/{emp_id}", response_model=schemas.Employee)
+def read_employee(emp_id: int, db: Session = Depends(get_db)):
+    emp = db.query(models.Employee).filter(models.Employee.id == emp_id).first()
+    if not emp:
+        raise HTTPException(status_code=404, detail="Employee not found")
+    return emp
+
+
+@app.put("/employees/{emp_id}", response_model=schemas.Employee)
+def update_employee(
+    emp_id: int, employee: schemas.EmployeeCreate, db: Session = Depends(get_db)
+):
+    db_emp = db.query(models.Employee).filter(models.Employee.id == emp_id).first()
+    if not db_emp:
+        raise HTTPException(status_code=404, detail="Employee not found")
+    for key, value in employee.model_dump().items():
+        setattr(db_emp, key, value)
+    db.commit()
+    db.refresh(db_emp)
+    return db_emp
+
+
+@app.delete("/employees/{emp_id}", status_code=204)
+def delete_employee(emp_id: int, db: Session = Depends(get_db)):
+    db_emp = db.query(models.Employee).filter(models.Employee.id == emp_id).first()
+    if not db_emp:
+        raise HTTPException(status_code=404, detail="Employee not found")
+    db.delete(db_emp)
+    db.commit()
+    return Response(status_code=204)
